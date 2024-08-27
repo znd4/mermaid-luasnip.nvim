7cb829ed4ffc74b87ad8e3de234966e44d672168
Zane Dufour <zane@protectai.com>
2024-08-27 12:50:19 -0400

    add fenced_code_block

--- lua/mermaid-luasnip/init.lua
+++ lua/mermaid-luasnip/init.lua
@@ -13,14 +13,14 @@ local treesitter_postfix = require("luasnip.extras.treesitter_postfix").treesitt
 local M = {}
 local match_node = {
   query = [[
-
+        (fenced_code_block
           (fenced_code_block_delimiter)
           (info_string
               (language) @variable.language
               (#eq? @variable.language "mermaid")
           )
           (block_continuation) @prefix
-
+        )
       ]],
   query_lang = "markdown",
 }
